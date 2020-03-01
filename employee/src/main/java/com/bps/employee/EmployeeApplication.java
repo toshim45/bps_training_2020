bb9bb5c913592d6977b2335d19b7d964756a7a06
Artiko Wibowo <artikow@gmail.com>
2020-03-01 17:11:48 +0700

    dual data source: mssql and mysql

--- employee/src/main/java/com/bps/employee/EmployeeApplication.java
+++ employee/src/main/java/com/bps/employee/EmployeeApplication.java
@@ -1,7 +1,14 @@
 package com.bps.employee;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class EmployeeApplication {
@@ -10,4 +17,25 @@ public class EmployeeApplication {
 		SpringApplication.run(EmployeeApplication.class, args);
 	}
 
+	@Bean(name = "mysqlDataSource")
+	@ConfigurationProperties("mysql.datasource")
+	public DataSource mysqlDataSource() {
+		return DataSourceBuilder.create().build();
+	}
+
+	@Bean(name = "mysqlJdbcTemplate")
+	public JdbcTemplate mysqlJdbcTemplate(@Qualifier("mysqlDataSource") DataSource ds) {
+		return new JdbcTemplate(ds);
+	}
+
+	@Bean(name = "mssqlDataSource")
+	@ConfigurationProperties("mssql.datasource")
+	public DataSource mssqlDataSource() {
+		return DataSourceBuilder.create().build();
+	}
+
+	@Bean(name = "mssqlJdbcTemplate")
+	public JdbcTemplate mssqlJdbcTemplate(@Qualifier("mssqlDataSource") DataSource ds) {
+		return new JdbcTemplate(ds);
+	}
 }
--- employee/src/main/java/com/bps/employee/repository/EmployeeRepository.java
+++ employee/src/main/java/com/bps/employee/repository/EmployeeRepository.java
@@ -4,16 +4,21 @@ import com.bps.employee.model.Employee;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
 
 @Repository
 public class EmployeeRepository {
     private Logger logger = LoggerFactory.getLogger(EmployeeRepository.class);
 
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
+    @Autowired @Qualifier("mssqlJdbcTemplate")
+    private JdbcTemplate mssqlJdbcTemplate;
+
+    @Autowired @Qualifier("mysqlJdbcTemplate")
+    private JdbcTemplate mysqlJdbcTemplate;
 
     @Transactional
     public void store(Employee employee){
@@ -22,7 +27,10 @@ public class EmployeeRepository {
                 " ON [Target].id = [Source].id" +
                 " WHEN MATCHED THEN  UPDATE SET [Target].reward = [Target].reward+1, [Target].updated = GetDate()" +
                 " WHEN NOT MATCHED THEN  INSERT (reward) VALUES (1);";
-        jdbcTemplate.update(query, employee.getId());
-        logger.debug("employee {} incremented ", employee.getId());
+        mssqlJdbcTemplate.update(query, employee.getId());
+        logger.debug("employee mssql {} incremented ", employee.getId());
+        String mysqlQuery = "INSERT INTO employee (id,reward) VALUES (?,1) ON DUPLICATE KEY UPDATE reward=reward+1";
+        mysqlJdbcTemplate.update(mysqlQuery, employee.getId());
+        logger.debug("employee mysql {} incremented ", employee.getId());
     }
 }
